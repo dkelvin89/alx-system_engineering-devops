4d811a09bdb2eb71d591d328f05e63320aa91af5
dkelvin89 <dkelvin89@outlook.com>
2023-03-02 11:09:38 +0100

    commit update

--- 0x06-regular_expressions/0-simply_match_school.rb
+++ 0x06-regular_expressions/0-simply_match_school.rb
@@ -2,4 +2,4 @@
 # shebang line - The first match in the $PATH environment variable.
 # Ruby script that accepts one argument and
 # pass it to a regular expression matching method
-puts ARGV[0].scan(/School/).join
+puts ARGV[0].scan(/Best School/).join
